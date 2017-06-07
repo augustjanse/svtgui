97011cc1cdfd2479c264856b5accf54fa820998f
augustjanse <augustj@kth.se>
2017-06-07 22:58:08 +0200

    Add useless GUI

---
+++ svtgui.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+
+# Very adapted from http://python-textbok.readthedocs.io/en/1.0/Introduction_to_GUI_Programming.html
+class SVTGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("SVTGUI")
+
+        self.label = tk.Label(master, text="Testtext här")
+        self.label.pack()
+
+        self.click_button = tk.Button(master, text="Klicka för att kalla funktion", command=self.do_something)
+        self.click_button.pack()
+
+        self.close_button = tk.Button(master, text="Klicka för att avsluta", command=master.quit)
+        self.click_button.pack()
+
+    def do_something(self):
+        print("få saker att hända här :)")
+
+
+root = tk.Tk()
+svtgui = SVTGUI(root)
+root.mainloop()
